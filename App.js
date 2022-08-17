d3392e1af8f6a667373c0e7ea3c8344de055526e
Umar <umarchothia@hotmail.co.uk>
2022-08-17 23:54:57 +0100

    base styling skeleton

--- App.js
+++ App.js
@@ -1,18 +1,26 @@
 import * as React from 'react';
-import { Text, View, StyleSheet } from 'react-native';
+import { Text, View, StyleSheet, SafeAreaView, Platform, StatusBar } from 'react-native';
 import Constants from 'expo-constants';
+import { colors } from './src/utils/colours';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Hello World!</Text>
-    </View>
+    <SafeAreaView style={styles.container}>
+      <Text style={styles.text}>Hello World!</Text>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    padding: 50,
+    paddingTop: Platform.OS === 'android' ? StatusBar.currentHeight : 0,
+    // backgroundColor: '#E1D9D1',
+    // backgroundColor: 'linear-gradient(to bottom, rgba(7,27,82,1) 0%, rgba(0,128,128,1) 100%);',
+    // backgroundColor: '#F8F0E3',
+    backgroundColor: colors.offWhite
+  },
+  text: {
+    color: colors.darkGrey
   }
 });
