c5c1f7d70282471741e9005357862a2b639c2d8b
Umar <umarchothia@hotmail.co.uk>
2022-08-18 06:56:17 +0100

    making button reactive

--- src/features/focus.js
+++ src/features/focus.js
@@ -4,15 +4,16 @@ import { TextInput } from 'react-native-paper';
 import { colors } from '../utils/colours';
 import { RoundedButton } from '../components/button';
 
-export const Focus = () => {
-const [subject, setSubject] = useState(null);
+// addTask is now a prop (property) of Focus
+export const Focus = ({addTask}) => {
+const [task, setTask] = useState(null);
 
 return (
     <View style={styles.container}>
       <View style={styles.inputContainer}>
       <TextInput 
       style={styles.textInput}
-      onChangeText={setSubject} 
+      onChangeText={setTask} 
       label="Today's Focus?"
       theme={{
          colors: {
@@ -23,7 +24,8 @@ return (
       }}
       />
       <View style={styles.button}>
-      <RoundedButton style={styles.buttonInside} title='+' size={50}/>
+      {/* onPress - When pressed call 'addTask', with out 'task' */}
+      <RoundedButton style={styles.buttonInside} title='+' size={50} onPress={() => addTask(task)}/>
       </View>
       </View>
     </View>
