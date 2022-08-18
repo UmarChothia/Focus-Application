98e8b5f226242489dc22537ad3bc303f260f3bb7
Umar <umarchothia@hotmail.co.uk>
2022-08-19 00:31:44 +0100

    start timer button

--- src/features/timer.js
+++ src/features/timer.js
@@ -1,17 +1,24 @@
-import React from "react";
+import React, { useState } from "react";
 import { View, StyleSheet } from "react-native";
 import { Countdown } from '../components/countdown';
+import { RoundedButton } from '../components/button';
 
-export const Timer = ({focusTask}) => (
+export const Timer = ({focusTask}) => {
+  const [hasStarted, setHasStarted] = useState(false);;
+  return (
   <View style={styles.container}>
     <View style={styles.countdown}>
-      <Countdown isPaused onProgress={() => {}} onEnd={() => {}}/>
+    {/* Countdown timer isPaused if the countdown has not been started  */}
+      <Countdown isPaused={!hasStarted} onProgress={() => {}} onEnd={() => {}}/>
     </View>
     <View style={styles.buttonWrapper}>
-
+{/* if it isn't started, show us the start button - AND if it is started show us the pause button */}
+      {!hasStarted && (<RoundedButton title="Go!" onPress={() => setHasStarted(true)}/>)}
+     {/* {hasStarted && (<RoundedButton title="pause" onPress={() => setHasStarted(false)}/>)}*/}
     </View>
   </View>
-)
+  ); 
+};
 
 const styles = StyleSheet.create({
   container: {
@@ -29,6 +36,5 @@ const styles = StyleSheet.create({
     padding: 15,
     justifyContent: 'center',
     alignItems: 'center',
-    backgroundColor: 'yellow',
   }
 })
