ddb18a2be55939b96569bc73841fe37cadd8c408
Umar <umarchothia@hotmail.co.uk>
2022-08-19 07:18:52 +0100

    task archive

--- src/features/focus.js
+++ src/features/focus.js
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 import { View, Text, StyleSheet } from 'react-native';
 import { TextInput } from 'react-native-paper';
 import { colors } from '../utils/colours';
@@ -6,45 +6,49 @@ import { RoundedButton } from '../components/button';
 import { paddings } from '../utils/sizes';
 
 // addTask is now a prop (property) of Focus
-export const Focus = ({addTask}) => {
-const [task, setTask] = useState(null);
+export const Focus = ({ addTask }) => {
+  const [task, setTask] = useState(null);
 
-return (
+  return (
     <View style={styles.container}>
       <View style={styles.inputContainer}>
-      <TextInput 
-      style={styles.textInput}
-      onChangeText={setTask} 
-      label="Today's Focus?"
-      theme={{
-         colors: {
-                    placeholder: colors.darkGrey, text: colors.darkGrey, primary: colors.darkGrey,
-                    underlineColor: 'transparent', 
-                    // background: '#003489'
-            }
-      }}
-      />
-      <View style={styles.button}>
-      {/* onPress - When pressed call 'addTask', with out 'task' */}
-      <RoundedButton style={styles.buttonInside} title='+' size={50} onPress={() => addTask(task)}/>
-      </View>
+        <TextInput
+          style={styles.textInput}
+          onChangeText={setTask}
+          label="Today's Focus?"
+          theme={{
+            colors: {
+              placeholder: colors.darkGrey,
+              text: colors.darkGrey,
+              primary: colors.darkGrey,
+              underlineColor: 'transparent',
+              // background: '#003489'
+            },
+          }}
+        />
+        <View style={styles.button}>
+          {/* onPress - When pressed call 'addTask', with out 'task' */}
+          <RoundedButton
+            style={styles.buttonInside}
+            title="+"
+            size={50}
+            onPress={() => addTask(task)}
+          />
+        </View>
       </View>
     </View>
   );
 };
 
-
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-  },
+  container: {},
 
   button: {
     justifyContent: 'center',
     paddingLeft: paddings.md,
   },
   buttonInside: {
-    justifyContent: 'center'
+    justifyContent: 'center',
   },
   textInput: {
     flex: 1,
@@ -53,6 +57,6 @@ const styles = StyleSheet.create({
   inputContainer: {
     padding: 25,
     justifyContent: 'top',
-    flexDirection: 'row'
+    flexDirection: 'row',
   },
 });
